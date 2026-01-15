57b86787e79b15a310d85c0d78f969b98cae3848
Pratik Jadhav <j.pratik025@gmail.com>
2026-01-15 19:21:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        
+        // Dutch National Flag Algorithm
+        // TC O(n) and SC O(1)
+        int low = 0, mid = 0, high = arr.size() - 1;
+        
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr[low], arr[mid]);
+                low++;
+                mid++;
+            }
+            else if (arr[mid] == 1) {
+                mid++;
+            }
+            else { 
+                swap(arr[mid], arr[high]);
+                high--;
+            }
+        }
+    }
+};
