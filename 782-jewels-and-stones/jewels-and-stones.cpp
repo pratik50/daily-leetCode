ca9246b735c55488fbface7e31c7d5b48c304341
Pratik Jadhav <j.pratik025@gmail.com>
2026-01-23 18:46:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (79.01%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         
-        unordered_set<char> J(jewels.begin(),jewels.end());
         int cnt = 0;
         for(char ch: stones){
-            if(J.count(ch))         //this returns bool
+            // npos --> no position, returns false.
+            if(jewels.find(ch) != string::npos)   
                 cnt++;
         }
         return cnt;
